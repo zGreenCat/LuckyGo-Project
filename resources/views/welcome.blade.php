238edb45a2cb7a69bcce078bbae592c6f5280061
nahuelNeculpan <nahuel21afkadi@gmail.com>
2024-06-28 19:51:04 -0400

    Detalles finales requerimientos USU-005 y SOR-002

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
                 <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">¿Ya jugaste?</h2>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">¿Ya tienes un billete? Si ya tienes un billete y quieres revisar cómo te fue en el sorteo, solo necesitas tener el número de billete e ingresarlo aquí abajo.</p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <a href="#" class="btn text-white bg-[#0A74DA] hover:bg-[#084A91] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 transition duration-200 justify-center items-center">
                 Ingresar billete
             
             </a>
